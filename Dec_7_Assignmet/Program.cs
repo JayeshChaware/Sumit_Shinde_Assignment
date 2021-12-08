422a9f4913b42abdf451b25f88acd3e4633daf7f
JayeshChaware <jayesh.chaware-imed@bvp.edu.in>
2021-12-09 02:55:41 +0530

    final string function

--- Dec_7_Assignmet/Program.cs
+++ Dec_7_Assignmet/Program.cs
@@ -6,7 +6,47 @@ namespace Dec_7_Assignmet
     {
         static void Main(string[] args)
         {
-            
+            StringManupulationFunctions SMF = new StringManupulationFunctions();
+            SMF.CloneString("jayesh");
+            SMF.CompareString("jayesh","chaware");
+            SMF.CompareOrdinalString("jayesh",3,"yesh",3,4);
+            SMF.CompareToString("jayesh","JayeSh");
+            SMF.ConcatString("jayesh ","chaware");
+            SMF.ContainsString("jayesh", "jay");
+            SMF.CopyString("jayesh");
+            SMF.CopyToString(2,"jayesh",3);
+            SMF.EndsWithString("jayesh","sh");
+            SMF.EqualsString("jayesh","jAyesh");
+            SMF.FormatString("jayesh");
+            SMF.GetHashCodeInt(3);
+            SMF.GetTypeCodeString("jayesh");
+            SMF.IndexOfString("jayesh",'s');
+            SMF.InsertString("jayesh");
+            SMF.InternString("jayesh");
+            SMF.IsNormalizedString("jayesh");
+            SMF.IsNullOrEmptyString(" ");
+            SMF.JoinString();
+            SMF.IsNullOrWhiteSpaceString();
+            SMF.LastIndexOfString("jayesh");
+            SMF.LastIndexOfAnyString("jayesh");
+            SMF.NormalizeString("jayesh");
+            SMF.PadLeftString("jayesh");
+            SMF.PadRightString("jayesh");
+            SMF.RemoveString("jayesh");
+            SMF.ReplaceString("jayesh");
+            SMF.SplitSpaceString("jayesh chaware");
+            SMF.SplitCommaString("jay,esh,chaw,are");
+            SMF.SplitCharString("jau=yesh chaware");
+            SMF.SubstringString("jayesh");
+            SMF.ToCharArrayString("jayesh");
+            SMF.ToLowerString("JayeSH");
+            SMF.ToLowerInvariantString("jayESh");
+            SMF.ToStringString(12363);
+            SMF.ToUpperString("jayesh");
+            SMF.TrimString("Jayesh");
+            SMF.TrimEndString("jayesh");
+            SMF.TrimStartString("jayesh");
+
         }
     }
 }
--- Dec_7_Assignmet/StringManupulationFunctions.cs
+++ Dec_7_Assignmet/StringManupulationFunctions.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 class StringManupulationFunctions
 {
-    private void CloneString(string inputString) //clone() example
+    public void CloneString(string inputString) //clone() example
     {
         string stringClone = (string)inputString.Clone();
         Console.WriteLine("Inputted string is: {0}\nClone String is: {1}\n", inputString, stringClone);
     }
 
-    private void CompareString(string inputStringOne, string inputStringTwo)
+    public void CompareString(string inputStringOne, string inputStringTwo)
     {
         int isSame = string.Compare(inputStringOne, inputStringTwo);
         if (isSame == 1)
@@ -18,7 +16,7 @@ class StringManupulationFunctions
             Console.WriteLine("{0} and {1} are different", inputStringOne, inputStringTwo);
     }
 
-    private void CompareOrdinalString(string inputStringOne, int inputIndexOne, string inputStringTwo, int inputIndexTwo, int length)
+    public void CompareOrdinalString(string inputStringOne, int inputIndexOne, string inputStringTwo, int inputIndexTwo, int length)
     {
         int isSame = string.CompareOrdinal(inputStringOne, inputIndexOne, inputStringTwo, inputIndexTwo, length);
         if (isSame == 1)
@@ -27,7 +25,7 @@ class StringManupulationFunctions
             Console.WriteLine("{0} and {1} dont have some part in common", inputStringOne, inputStringTwo);
     }
 
-    private void CompareToString(string inputStringOne, string inputStringTwo)
+    public void CompareToString(string inputStringOne, string inputStringTwo)
     {
         int isSame = inputStringOne.CompareTo(inputStringTwo);
         if (isSame == 1)
@@ -36,13 +34,13 @@ class StringManupulationFunctions
             Console.WriteLine("{0} and {1} are different", inputStringOne, inputStringTwo);
     }
 
-    private void ConcatString(string inputStringOne, string inputStringTwo)
+    public void ConcatString(string inputStringOne, string inputStringTwo)
     {
         string concatedString = string.Concat(inputStringOne, inputStringTwo);
         Console.WriteLine("after concating {0} and {1} we get: {2}", inputStringOne, inputStringTwo, concatedString);
     }
 
-    private void ContainsString(string inputStringOne, string inputStringTwo)
+    public void ContainsString(string inputStringOne, string inputStringTwo)
     {
         bool isContains = inputStringOne.Contains(inputStringTwo);
         if (isContains)
@@ -51,13 +49,13 @@ class StringManupulationFunctions
             Console.WriteLine("{0} does not contains {1} ", inputStringOne, inputStringTwo);
     }
 
-    private void CopyString(string inputStringOne)
+    public void CopyString(string inputStringOne)
     {
         string stringCopy = string.Copy(inputStringOne);
         Console.WriteLine("original sytring: {0} \ncopy string: {1}", inputStringOne, stringCopy);
     }
 
-    private void CopyToString(int startIndex, string inputStringOne, int totalIndex)
+    public void CopyToString(int startIndex, string inputStringOne, int totalIndex)
     {
         char[] stringCopy = new char[totalIndex];
         inputStringOne.CopyTo(startIndex, stringCopy, 0, totalIndex);
@@ -68,7 +66,7 @@ class StringManupulationFunctions
         }
     }
 
-    private void EndsWithString(string inputStringOne, string inputStringTwo)
+    public void EndsWithString(string inputStringOne, string inputStringTwo)
     {
         bool isEndsWith = inputStringOne.EndsWith(inputStringTwo);
         if (isEndsWith)
@@ -77,7 +75,7 @@ class StringManupulationFunctions
             Console.WriteLine("{0} does not ends with {1} ", inputStringOne, inputStringTwo);
     }
 
-    private void EqualsString (string inputStringOne, string inputStringTwo)
+    public void EqualsString(string inputStringOne, string inputStringTwo)
     {
         bool isEquals = Equals(inputStringTwo, inputStringOne);
         if (isEquals)
@@ -86,44 +84,44 @@ class StringManupulationFunctions
             Console.WriteLine("{0} and {1} are not equal", inputStringOne, inputStringTwo);
     }
 
-    private void FormatString(string inputStringOne)
+    public void FormatString(string inputStringOne)
     {
         string formatString;
-        formatString = string.Format("example of {0} method",inputStringOne);
+        formatString = string.Format("example of {0} method", inputStringOne);
         Console.WriteLine(formatString);
     }
 
-    private void GetEnumeratorString()
+    public void GetEnumeratorString()
     {
         //ask
     }
 
-    private void GetHashCodeInt(int inputNumber)
+    public void GetHashCodeInt(int inputNumber)
     {
-        int hasgCode=inputNumber.GetHashCode();
-        Console.WriteLine("hash code of {0} is: {1}",inputNumber,hasgCode);
+        int hasgCode = inputNumber.GetHashCode();
+        Console.WriteLine("hash code of {0} is: {1}", inputNumber, hasgCode);
     }
 
-    private void GetTypeDataType(string inputData)
+    public void GetTypeDataType(string inputData)
     {
         Console.WriteLine(inputData.GetType());
     }
 
-    private void GetTypeCodeString(string inputData)
+    public void GetTypeCodeString(string inputData)
     {
         Console.WriteLine(inputData.GetTypeCode());
     }
 
-    private void IndexOfString(string inputString, char searcChar)
+    public void IndexOfString(string inputString, char searcChar)
     {
         int indexOfElement = inputString.IndexOf(searcChar);
     }
 
-    private void InsertString(string inputString)
+    public void InsertString(string inputString)
     {
         Console.WriteLine("Inserted : {0}", inputString.Insert(4, "SB"));
     }
-    private void InternString(string inputString)
+    public void InternString(string inputString)
     {
         string internString = string.Intern(inputString);
         string isInternedString = string.IsInterned(inputString);
@@ -132,102 +130,132 @@ class StringManupulationFunctions
         Console.WriteLine("IsInterned : {0}", isInternedString);
     }
 
-    private void IsNormalizedString(string inputString)
+    public void IsNormalizedString(string inputString)
     {
         Console.WriteLine("IsNormalized : {0}", inputString.IsNormalized());
     }
-    private void IsNullOrEmptyString(string inputString)
+    public void IsNullOrEmptyString(string inputString)
     {
         Console.WriteLine("IsNullOrEmpty : {0}", string.IsNullOrEmpty(inputString));
     }
-    private void JoinString()
+    public void JoinString()
     {
-        object[] objectArray = { "Jayesh","Chaware"};
-        string joinString = string.Join(" ",objectArray);
-        Console.WriteLine("value after join: {0}",joinString);
+        object[] objectArray = { "Jayesh", "Chaware" };
+        string joinString = string.Join(" ", objectArray);
+        Console.WriteLine("value after join: {0}", joinString);
     }
-    private void IsNullOrWhiteSpaceString()
+    public void IsNullOrWhiteSpaceString()
     {
-        string[] demoValueString = { null, "100", " ", " ", new string(' ',10), "jayesh"};
+        string[] demoValueString = { null, "100", " ", " ", new string(' ', 10), "jayesh" };
         foreach (string element in demoValueString)
             Console.WriteLine(string.IsNullOrWhiteSpace(element));
     }
-    private void LastIndexOfString(string inputString)
+    public void LastIndexOfString(string inputString)
     {
-        Console.WriteLine(inputString.LastIndexOf)
+        Console.WriteLine(inputString.LastIndexOf('h'));
     }
-    private void LastIndexOfAnyString()
+    public void LastIndexOfAnyString(string inputString)
+    {
+        char[] ch = { 'h', 'e' };
+        Console.WriteLine(inputString.LastIndexOfAny(ch));
+    }
+    public void NormalizeString(string inputString)
+    {
+        Console.WriteLine("Normalize : {0} ", inputString.Normalize());
+    }
+    public void PadLeftString(string inputString)
+    {
+        Console.WriteLine("Pad Left : {0} ", inputString.PadLeft(5));
+    }
+    public void PadRightString(string inputString)
+    {
+        Console.WriteLine("Pad Right : {0} ", inputString.PadRight(6));
+    }
+    public void RemoveString(string inputString)
+    {
+        Console.WriteLine("Remove : {0} ", inputString.Remove(3));
+    }
+    public void ReplaceString(string inputString)
+    {
+        Console.WriteLine("Replaced : {0} ", inputString.Replace('e', 'z'));
+    }
+    public void SplitSpaceString(string inputString)
+    {
+        string[] splittedString = inputString.Split(' ');
+        Console.WriteLine("Split using whitespace : ");
+        foreach (string individualString in splittedString)
+        {
+            Console.WriteLine(individualString);
+        }
+    }
+    public void SplitCommaString(string inputString)
+    {
+        string[] splittedString = inputString.Split(',');
+        Console.WriteLine("Split using comma : ");
+        foreach (string individualString in splittedString)
+        {
+            Console.WriteLine(individualString);
+        }
+    }
+
+    public void SplitCharString(string inputString)
+    {
+        string[] splittedString = inputString.Split('a');
+        Console.WriteLine("Split using character : ");
+        foreach (string individualString in splittedString)
+        {
+            Console.WriteLine(individualString);
+        }
+    }
+    public void StartsWithString(string inputString)
+    {
+        Console.WriteLine("StartsWith : {0} ", inputString.StartsWith("S"));
+    }
+    public void SubstringString(string inputString)
+    {
+        Console.WriteLine("Substring : {0} ", inputString.Substring(3));
+    }
+    public void ToCharArrayString(string inputString)
+    {
+        char[] stringToChar = inputString.ToCharArray();
+
+        Console.WriteLine("String: " + inputString);
+        Console.Write("Character array :");
+        foreach (char i in stringToChar)
+        {
+            Console.Write("{0} ", i);
+        }
+    }
+
+    public void ToLowerString(string inputString)
+    {
+        Console.WriteLine("{0} to lower {1}",inputString,inputString.ToLower());
+    }
+    public void ToLowerInvariantString(string inputString)
+    {
+        Console.WriteLine("{0} to lower invariant {1}", inputString, inputString.ToLowerInvariant());
+    }
+    public void ToStringString(int inputNumber)
+    {
+        Console.WriteLine("to string demo: {0}",inputNumber.ToString());
+    }
+    public void ToUpperString(string inputString)
+    {
+        Console.WriteLine("{0} to upper {1}", inputString, inputString.ToUpper());
+    }
+    public void TrimString(string inputString)
     {
 
+        Console.WriteLine("{0} after trim {1}", inputString, inputString.Trim('j'));
     }
-    private void NormalizeString()
+    public void TrimEndString(string inputString)
     {
-
+        Console.WriteLine("{0} after trim start{1}", inputString, inputString.TrimStart('a'));
     }
-    private void PadLeftString()
+    public void TrimStartString(string inputString)
     {
-
+        Console.WriteLine("{0} after trim end {1}", inputString, inputString.TrimEnd('y'));
     }
-    private void PadRightString()
-    {
 
-    }
-    private void RemoveString()
-    {
-
-    }
-    private void ReplaceString()
-    {
-
-    }
-    private void SplitString()
-    {
-
-    }
-    private void StartsWithString()
-    {
-
-    }
-    private void SubstringString()
-    {
-
-    }
-    private void ToCharArrayString()
-    {
-
-    }
-    private void String()
-    {
-
-    }
-    private void ToLowerString()
-    {
-
-    }
-    private void ToLowerInvariantString()
-    {
-
-    }
-    private void ToStringString()
-    {
-
-    }
-    private void ToUpperString()
-    {
-
-    }
-    private void TrimString()
-    {
-
-    }
-    private void TrimEndString()
-    {
-
-    }
-    private void TrimStartString()
-    {
-
-    }
-  
 
 }
