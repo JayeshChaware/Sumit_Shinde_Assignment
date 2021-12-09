8260b9d7feb98275b8fa4d4cc65865e1aeecfd31
JayeshChaware <jayesh.chaware-imed@bvp.edu.in>
2021-12-09 08:23:40 +0530

    string builder

---
+++ StringBuilderFuncton/StringBuilderMethods.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System;
+namespace StringBuilderFuncton
+{
+    public class StringBuilderMethods
+    {
+        public void AppendStringBuilder(StringBuilder inputStingBuilder)
+        {
+            Console.WriteLine("Append functoin: {0}", inputStingBuilder.Append(" Hello World!"));
+        }
+
+        public void AppendFormatStringBuilder(StringBuilder inputStringBuilder)
+        {
+            Console.WriteLine("AppendFormat function: {0}", inputStringBuilder.AppendFormat(" Hello World!"));
+        }
+
+        public void InsertStringBuilder(StringBuilder inputStringBuilder)
+        {
+            Console.WriteLine("Insert function: {0}", inputStringBuilder.Insert(6, " Hello World"));
+        }
+
+        public void RemoveStringBuilder(StringBuilder inputStringBuilder)
+        {
+            Console.WriteLine("Remove function: {0}", inputStringBuilder.Remove(6, 3));
+        }
+
+        public void ReplaceStringBuilder(StringBuilder inputStringBuilder)
+        {
+            Console.WriteLine("Replace function: {0}", inputStringBuilder.Replace("jay", "jayesh Chaware "));
+        }
+    }
+}
