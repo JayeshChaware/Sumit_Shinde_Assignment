d3ef3d9ad128fb858197dae99b3ecee3e32461ee
JayeshChaware <jayesh.chaware-imed@bvp.edu.in>
2021-12-09 09:04:41 +0530

    exceptions handling

--- DelegateEvetsAndReflection/Program.cs
+++ DelegateEvetsAndReflection/Program.cs
@@ -54,6 +54,19 @@ namespace DelegateEvetsAndReflection
              */
 
 
+            /* How to Use Events and Delegates in C#? and write a sample program using events
+             * Events are user actions such as key press, clicks, mouse movements, etc., 
+             * or some occurrence such as system generated notifications.
+             * Applications need to respond to events when they occur.
+             * For example, interrupts. Events are used for inter-process communication.
+             
+             */
+            /* What is reflection ? and write a program to use reflection.
+             * Reflection provides objects (of type Type) that describe assemblies, modules, and types. 
+             *You can use reflection to dynamically create an instance of a type, bind the type to an existing object, or get the type from an existing object and invoke its methods or access its fields and properties.
+             *If you are using attributes in your code, reflection enables you to access them. 
+             */
+
         }
     }
 }
---
+++ ExceptionHandling/ExeptionHandlingClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    public class ExeptionHandlingClass
+    {
+        public void ExceptionHandlingQuestionThree()
+        {
+            int[] arr = { 1 };
+            
+
+            try {
+                Console.WriteLine("{0}/0={1}", arr[0], arr[0] / 0);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division of {0} by zero.", arr[0]);
+            }
+
+            try
+            {
+                arr[1] = 100;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Cant add element in array. Array is full");
+            }
+            try
+            {
+                arr[0] = (int)'j';
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+
+        public void ExceptionHandlingQuestionFour()
+        {
+            int[] arr = { 1 };
+            try
+            {
+                Console.WriteLine("{0}/0={1}", arr[0], arr[0] / 0);
+                arr[1] = 100;
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division of {0} by zero.", arr[0]);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Cant add element in array. Array is full");
+            }
+            finally 
+            {
+                Console.WriteLine("program Executed");
+            }
+
+        }
+    }
+}
